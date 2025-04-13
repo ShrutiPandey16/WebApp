7df515205886191f6328804d6cd1a77290034349
shrutiPandey <shrutipandey23456@gmail.com>
2025-04-13 22:56:54 +0530

    connecting success page 2

--- src/main/java/com/telusko/JobApp/controller/JobController.java
+++ src/main/java/com/telusko/JobApp/controller/JobController.java
@@ -9,28 +9,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JobController {
 
-    @GetMapping({ "/", "home" })
+    @Autowired
+    private JobService service;
+
+
+    @GetMapping({"/", "home"})
     public String home() {
-    	System.out.println("Inside home");
         return "home";
     }
 
+
     @GetMapping("addjob")
     public String addJob() {
-    	System.out.println("Inside addJob");
         return "addjob";
     }
 
+
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost) {
-//        service.addJob(jobPost);
+        service.addJob(jobPost);
         return "success";
-    }  
-}    
+
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model m) {
+        List<JobPost> jobs = service.getAllJobs();
+        m.addAttribute("jobPosts", jobs);
+
+        return "viewalljobs";
+    }
+
+}
